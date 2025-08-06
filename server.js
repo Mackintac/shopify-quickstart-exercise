ffc7df4bf74cb16dce92343f4a4cea508695c9c7
Mackenzie Graungaard-Robinson <47095797+Mackintac@users.noreply.github.com>
2025-08-06 17:56:09 -0400

    remove legacy cors

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const dotenv = require('dotenv');
-const cors = require('cors');
 
 dotenv.config();
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
